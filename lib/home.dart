4e76cadc5822302637f22d76bd842824ca1f2bc8
Eddy <eddypretorius@gmail.com>
2020-03-28 21:18:07 +0200

    commit

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:qrscan/qrscan.dart' as scanner;
 
 class HomePage extends StatefulWidget {
   @override
@@ -7,24 +8,54 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String cameraScanResult = "test";
+  Future scan() async {
+    var scannerResult = await scanner.scan();
+    setState(() {
+      this.cameraScanResult = scannerResult;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child:  Center(
-        child: GestureDetector(
-          onTap: () {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
 
-          },
-          child:  CircleAvatar(
-            radius: 55.0,
-            backgroundColor: Colors.red,
-            child: Text(
-                'Scorch'
-            ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(cameraScanResult),
+                ),
+              ),
+            ],
           ),
-        ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                child: Center(
+                  child: GestureDetector(
+                    onTap: () {
+                      scan();
+                    },
+                    child:  CircleAvatar(
+                      radius: 55.0,
+                      backgroundColor: Colors.red,
+                      child: Text(
+                          'Scorch'
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ],
       ),
-
     );
   }
 }
